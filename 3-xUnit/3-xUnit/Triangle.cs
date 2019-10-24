3fe8971c8efe9a888968439fa1171172fe84ab41
Stanislau Pyrkin <stanislau.pyrkin@gmail.com>
2019-10-24 19:16:59 +0300

    xUnit+testing

---
+++ 3-xUnit/3-xUnit/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_xUnit
+{
+    public class Triangle
+    {
+        public bool TriangleInequality(double a, double b, double c)
+        {
+            if (a > 0 & b > 0 & c > 0)
+            {
+                if (a <= (b + c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 3-xUnit/XUnitTestProject/TrinagleTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _3_xUnit;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class TrinagleTest
+    {
+        [Fact]
+        public void TriangleInequalityEquilateralTest()
+        {
+            Assert.True(new Triangle().TriangleInequality(5, 5, 5));
+        }
+
+        [Fact]
+        public void TriangleInequalityZeroTest()
+        {
+            Assert.False(new Triangle().TriangleInequality(0, 0, 0));
+        } 
+
+        [Fact]
+        public void TriangleInequalityExistTest()
+        {
+            Assert.True(new Triangle().TriangleInequality(3, 4, 5));
+        }
+        [Fact]
+        public void TriangleInequalityNotExist()
+        {
+            Assert.False(new Triangle().TriangleInequality(8, 1, 1));
+        }
+        [Fact]
+        public void TriangleInequalityIsosceles()
+        {
+            Assert.True(new Triangle().TriangleInequality(4, 2, 2));
+        }
+        [Fact]
+        public void TriangleInequalityOneBadSideTest()
+        {
+            Assert.False(new Triangle().TriangleInequality(-3, 4, 5));
+        }
+        [Fact]
+        public void TriangleInequalityTwoBadSidesTest()
+        {
+            Assert.False(new Triangle().TriangleInequality(-3, -4, 5));
+        }
+        [Fact]
+        public void TriangleInequalityThreeBadSidesTest()
+        {
+            Assert.False(new Triangle().TriangleInequality(-11, -4, -5));
+        }
+        [Fact]
+        public void TriangleInequalityDoubleValuesTest()
+        {
+            Assert.True(new Triangle().TriangleInequality(3.5, 4.1, 5.3));
+        }
+        [Fact]
+        public void TriangleInequalityOneLeftZeroTest()
+        {
+            Assert.False(new Triangle().TriangleInequality(0, 4, 6));
+        }
+    }
+}
