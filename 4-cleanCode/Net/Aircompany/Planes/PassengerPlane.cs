b32b46cf9abc21836798b2f43220a3feb3d89c2e
Stanislau Pyrkin <stanislau.pyrkin@gmail.com>
2019-11-05 11:08:31 +0300

    code cleaned

--- 4-cleanCode/Net/Aircompany/Planes/PassengerPlane.cs
+++ 4-cleanCode/Net/Aircompany/Planes/PassengerPlane.cs
@@ -4,42 +4,37 @@ namespace Aircompany.Planes
 {
     public class PassengerPlane : Plane
     {
-        public int _passengersCapacity;
+        private readonly int _passengersCapacity;
 
         public PassengerPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity)
-            :base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
         {
             _passengersCapacity = passengersCapacity;
         }
 
         public override bool Equals(object obj)
         {
-            var plane = obj as PassengerPlane;
-            return plane != null &&
+            return obj is PassengerPlane plane &&
                    base.Equals(obj) &&
                    _passengersCapacity == plane._passengersCapacity;
         }
 
         public override int GetHashCode()
         {
-            var hashCode = 751774561;
-            hashCode = hashCode * -1521134295 + base.GetHashCode();
-            hashCode = hashCode * -1521134295 + _passengersCapacity.GetHashCode();
-            return hashCode;
+            const int coefficient = -1521134295;
+            return (unchecked(751774561 * coefficient) + base.GetHashCode()) * coefficient + _passengersCapacity.GetHashCode();
         }
 
-        public int PassengersCapacityIs()
+        public int GetCapacity()
         {
             return _passengersCapacity;
         }
 
-       
         public override string ToString()
         {
             return base.ToString().Replace("}",
                     ", passengersCapacity=" + _passengersCapacity +
                     '}');
-        }       
-        
+        }
     }
 }
